ae1c443bc44e63fe82448fc76d0e2a00e4143bb3
Joshua <48163249+JoshuaP199@users.noreply.github.com>
2022-07-04 12:58:28 -0400

    Finished

--- main.js
+++ main.js
@@ -1,24 +1,108 @@
-const canvas = document.getElementById("myCanvas");
-canvas.width = 200;
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width = 200;
 
-const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width/2, canvas.width*0.9);
-const car = new Car(road.getLaneCenter(1),100,30,50);
+// const networkCanvas = document.getElementById("networkCanvas");
+// networkCanvas.width = 300;
+
+const carCtx = carCanvas.getContext("2d");
+//const networkCtx = networkCanvas.getContext("2d");
+
+const road = new Road(carCanvas.width/2, carCanvas.width*0.9);
+
+const N=500;
+const cars = generateCars(N);
+let bestCar=cars[0];
+if(localStorage.getItem("bestBrain")){
+    for(let i=0;i<cars.length;i++){
+        cars[i].brain=JSON.parse(localStorage.getItem("bestBrain"));
+        if(i!=0){
+            NeuralNetwork.mutate(cars[i].brain,0.1);
+        }
+    }   
+}
+
+const traffic = [
+    new Car(road.getLaneCenter(1), -100,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0), -300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2), -300,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1), -500,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(3), -650,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0), -700,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1), -800,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2), -900,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0), -1000,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2), -1000,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(0), -1100,30,50,"DUMMY",2)
+];
 
 animate();
 
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+// function generateCars(number){
+//     for(let i=0;i<=number;i++){
+//         new Car(road.getLaneCenter(getRandomIntInclusive(0,2), #,#,#,"DUMMY",2);
+//     }
+// }
+
+function save(){
+    localStorage.setItem("bestBrain",
+        JSON.stringify(bestCar.brain));
+}
+
+function discard(){
+    localStorage.removeItem("bestBrain");
+}
+
+function generateCars(N){
+    const cars=[];
+    for(let i=1;i<=N;i++){
+        cars.push(new Car(road.getLaneCenter(1),100,30,50,"AI"));
+    }
+    return cars;
+}
+
 function animate(){
-    car.update(road.borders);
+    for(let i =0;i<traffic.length;i++){
+        traffic[i].update(road.borders,[]);
+    }
+    for(let i=0;i<cars.length;i++){
+        cars[i].update(road.borders,traffic);
+    }
+    
+    bestCar = cars.find(
+        c=>c.y==Math.min(
+            ...cars.map(c=>c.y)
+        )
+    );
 
     //Makes it so that the car looks like its moving and not just making a line
-    canvas.height = window.innerHeight; 
+    carCanvas.height = window.innerHeight; 
 
-    ctx.save();
-    ctx.translate(0,-car.y+canvas.height*0.7);
+    //networkCanvas.height = window.innerHeight; 
 
-    road.draw(ctx);
-    car.draw(ctx);
+    carCtx.save();
+    carCtx.translate(0,-bestCar.y+carCanvas.height*0.7);
+
+    road.draw(carCtx);
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].draw(carCtx,"red");
+    }
+
+    carCtx.globalAlpha=0.2;
+
+    for(let i=0;i<cars.length;i++){
+        cars[i].draw(carCtx,"blue");
+    }
+
+    carCtx.globalAlpha=1;
+    bestCar.draw(carCtx,"blue", true);
+    //console.log(bestCar.brain);
     
-    ctx.restore();
+    carCtx.restore();
+
     requestAnimationFrame(animate)
 }
--- sensor.js
+++ sensor.js
@@ -1,25 +1,25 @@
 class Sensor{
     constructor(car){
         this.car = car;
-        this.rayCount=4;
+        this.rayCount=5;
         this.rayLength=150;
-        this.raySpread=Math.PI/3;
+        this.raySpread=Math.PI/2;
 
         this.rays=[];
         this.readings=[];
     }
 
-    update(roadBorders){
+    update(roadBorders,traffic){
         this.#castRays();
         this.readings=[];
         for(let i=0;i<this.rays.length;i++){
             this.readings.push(
-                this.#getReading(this.rays[i],roadBorders)
+                this.#getReading(this.rays[i],roadBorders,traffic)
             );
         }
     }
 
-    #getReading(ray,roadBorders){
+    #getReading(ray,roadBorders,traffic){
         let touches = [];
 
         for(let i = 0; i<roadBorders.length;i++){
@@ -31,6 +31,21 @@ class Sensor{
             );
             if(touch){
                 touches.push(touch);
+            }
+        }
+
+        for(let i = 0; i<traffic.length;i++){
+            const poly=traffic[i].polygon;
+            for(let j=0;j<poly.length;j++){
+                const value = getIntersection(
+                    ray[0],
+                    ray[1],
+                    poly[j],
+                    poly[(j+1)%poly.length]
+                );
+                if(value){
+                    touches.push(value);
+                }
             }
         }
 
